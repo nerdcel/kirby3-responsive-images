ab06f58ac88472d720b11bf9d671240dcce24923
Marcel Hieke <office@marcelhieke.com>
2023-05-25 14:41:15 +0200

    update docblock

--- src/helpers.php
+++ src/helpers.php
@@ -7,11 +7,14 @@ if (!function_exists('responsiveImage')) {
     /**
      * Creates a responsive image based on slug from a kirby file
      *
-     * @param string $responsiveImageSlug
-     * @param File $file
+     * @param  string  $responsiveImageSlug
+     * @param  File  $file
      * @param  string|null  $classes
+     * @param  bool  $lazy
+     * @param  null  $alt
      *
-     * @return void
+     * @return string
+     * @throws Exception
      */
     function responsiveImage(string $responsiveImageSlug, File $file, string $classes = null, $lazy = false, $alt = null): string
     {
