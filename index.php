fd76f50367779d6a929ee69677025d8186049efb
Marcel Hieke-Hecht <office@marcelhieke.com>
2025-12-05 16:09:13 +0100

    feat: update getkirby/cms to version 5.1.4 and add responsive image settings retrieval

--- index.php
+++ index.php
@@ -1,30 +1,41 @@
 <?php
 
-@include_once __DIR__ . '/vendor/autoload.php';
+use Nerdcel\ResponsiveImages\ResponsiveImages;
+
+@include_once __DIR__.'/vendor/autoload.php';
 
 Kirby::plugin('nerdcel/responsive-images', [
-    'options' => require __DIR__ . '/src/config.php',
+    'options' => require __DIR__.'/src/config.php',
 
     'translations' => [
-        'en' => require __DIR__ . '/i18n/en.php',
-        'de' => require __DIR__ . '/i18n/de.php'
+        'en' => require __DIR__.'/i18n/en.php',
+        'de' => require __DIR__.'/i18n/de.php',
     ],
 
     'areas' => [
-        'responsiveimages' => require __DIR__ . '/src/Areas/ResponsiveImages.php',
+        'responsiveimages' => require __DIR__.'/src/Areas/ResponsiveImages.php',
     ],
 
     'api' => [
-        'routes' => require 'src/routes.php'
+        'routes' => require 'src/routes.php',
     ],
 
     'permissions' => [
-        'access' => true
+        'access' => true,
     ],
 
     'fields' => [
-        'focalpoints' => require __DIR__ . '/src/Fields/FocalPoints.php',
+        'focalpoints' => require __DIR__.'/src/Fields/FocalPoints.php',
     ],
 
-    'fieldMethods' => require __DIR__ . '/src/fieldMethods.php',
+    'fieldMethods' => require __DIR__.'/src/fieldMethods.php',
+
+    'siteMethods' => [
+        'getResponsiveImagesSetting' => function ($slug = null) {
+            if ($slug) {
+                return (new ResponsiveImages(kirby()))->getSlugConfig($slug);
+            }
+            return 'No settings found';
+        },
+    ],
 ]);
